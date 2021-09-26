37b80e88abb91346687275f68fec142622fb7655
Brooks Patton <brooks@brookzerker.com>
2021-09-26 12:46:48 -0600

    borrow trait

---
+++ examples/borrow_trait.rs
@@ -0,0 +1,24 @@
+use std::{borrow::Borrow, collections::HashMap};
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct WrappedString {
+    value: String,
+}
+
+impl Borrow<str> for WrappedString {
+    fn borrow(&self) -> &str {
+        self.value.as_str()
+    }
+}
+
+fn main() {
+    let mut entities: HashMap<WrappedString, u32> = HashMap::new();
+    entities.insert(
+        WrappedString {
+            value: "foo".to_string(),
+        },
+        42,
+    );
+    let foo = entities.get("foo");
+    dbg!(foo);
+}
