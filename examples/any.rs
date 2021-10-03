7e70117b23a524085b390e373c26742fcb67bfb3
Brooks Patton <BrooksPatton@users.noreply.github.com>
2021-10-03 18:23:11 +0000

    Any

---
+++ examples/any.rs
@@ -0,0 +1,29 @@
+use std::{
+    any::{Any, TypeId},
+    fmt::Debug,
+};
+
+#[derive(Debug)]
+struct Location {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Debug)]
+struct Locationy<'a> {
+    location: &'a Location,
+}
+
+fn print_debug<T: Any + Debug>(thing: &T) {
+    dbg!(thing, thing.type_id());
+}
+
+fn main() {
+    // this is static, so can be sent to print_debug just fine
+    let location = Location { x: 100.0, y: 200.0 };
+    // &location is not static, so therefore cannot be sent to print_debug
+    let _stringy = Locationy {
+        location: &location,
+    };
+    print_debug(&location);
+}
