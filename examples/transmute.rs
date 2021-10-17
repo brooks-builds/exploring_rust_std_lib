efc250194189d11f8c36a4eec6e17104e7aec307
Brooks Patton <BrooksPatton@users.noreply.github.com>
2021-10-17 15:55:26 +0000

    transmute will come later

---
+++ examples/transmute.rs
@@ -0,0 +1,16 @@
+use std::intrinsics::transmute;
+
+fn main() {
+    // print_size_of_type::<u32>();
+    // print_size_of_type::<f32>();
+    // print_size_of_type::<i32>();
+    // print_size_of_type::<f64>();
+    // print_size_of_type::<u64>();
+    // print_size_of_type::<char>();
+    let result: char = unsafe { transmute(100_u32) };
+    dbg!(result);
+}
+
+fn print_size_of_type<T>() {
+    dbg!(std::mem::size_of::<T>());
+}
